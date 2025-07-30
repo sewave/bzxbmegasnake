0bebb101866479ad7da680c5118b7b86f889ab11
Sergio López Cantero <sewave@gmail.com>
2025-07-30 21:11:03 +0200

    Title music v1

--- processAssets.bat
+++ processAssets.bat
@@ -1,2 +1,2 @@
 python .\tools\convertEffects.py .\assets\audio\effects.asm .\src\game\audio\effects.zxbas
-python .\tools\convertMusic.py .\assets\audio\intro_music.asm .\src\game\audio\intro_music.zxbas intro
+python .\tools\convertMusic.py .\assets\audio\title.asm .\src\game\audio\title.zxbas title
